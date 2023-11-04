00b667dc17b36a37ec0cdee610e37f979b8adfda
Admin <duchandsome0201@gmail.com>
2023-11-05 02:19:47 +0700

    Initial commit

---
+++ A-Star-Algorithm/src/alogrithm/Main.java
@@ -0,0 +1,7 @@
+package alogrithm;
+
+public class Main {
+    public static void main(String[] args) {
+        Algorithm4Direction alogrithm = new Algorithm4Direction(Algorithm4Direction.EXTRA_LARGE);
+    }
+}
